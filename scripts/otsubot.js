151682bf8bcc3472c480846318183979aca37b83
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-09-11 16:18:06 +0900

    デバッグ文を削除

--- scripts/otsubot.js
+++ scripts/otsubot.js
@@ -42,7 +42,6 @@ module.exports = function (robot) {
 
     function listCommand(msg) {
         try {
-            console.log(msg);
             var user = msg.message.user.name;
             if (msg.match[1]) {
                 user = msg.match[1].replace(/^@/, '');
