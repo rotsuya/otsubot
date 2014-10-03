0920e08a8f13555389cb1d2086516dd331816494
Ryosuke Otsuya <r.otsuya@gmail.com>
2014-10-03 13:19:14 +0900

    #6 (hi|bye) -u userid が動かないバグに対応

--- scripts/otsubot.js
+++ scripts/otsubot.js
@@ -47,11 +47,11 @@ module.exports = function (robot) {
     var MILLISEC_PER_HOUR = 60 * 60 * 1000;
     var MILLISEC_PER_MINUTE =  60 * 1000;
 
-    robot.hear(/^list(?: -u ([^\s]+))?(?: (?:(\d{2}|\d{4})\/?)?(\d{1,2}))?$/i, listCommand);
+    robot.hear(/^list(?: -u ([^\s]+))?(?: (?:(\d{2}|\d{4})\/?)?(\d{1,2}))? *$/i, listCommand);
 
-    robot.hear(/^(?:hi|hello|おはよう\S*) ?(?:-u ([^\s]+) )?(?:([\d/]+) )?(?:([\d:]+)-?)?(?:-([\d:]+))?$/i, hiByeCommand('hi'));
+    robot.hear(/^(?:hi|hello|おは\S*)(?: -u ([^\s]+))?(?:(?: ([\d\/]+))?(?: (?:([\d:]+)-?)(?:-([\d:]+))?))? *$/i, hiByeCommand('hi'));
 
-    robot.hear(/^(?:bye|お疲れ\S*|乙) ?(?:-u ([^\s]+) )?(?:([\d/]+) )?(?:([\d:]+)-)?(?:-?([\d:]+))?$/i, hiByeCommand('bye'));
+    robot.hear(/^(?:bye|お疲れ\S*|乙|さよ\S*)(?: -u ([^\s]+))?(?:(?: ([\d\/]+))?(?: (?:([\d:]+)-)?(?:-?([\d:]+))))? *$/i, hiByeCommand('bye'));
 
     function listCommand(msg) {
         try {
